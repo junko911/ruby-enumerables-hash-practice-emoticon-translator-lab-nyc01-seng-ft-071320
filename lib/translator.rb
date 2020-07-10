c35d7ea2988001653e92942335684f843c843345
Junko Tahara <junkotahara911@gmail.com>
2020-07-10 17:00:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,13 +3,13 @@ require 'pry'
 
 def load_library
   emoticons = {}
-  YAML.load_file('lib/emoticons.yml').size
-  # data.each { |meaning, faces|
-  #   emoticons[meaning] = {
-  #     :english => faces[0],
-  #     :japanese => faces[1]
-  # }
-  # emoticons
+  YAML.load_file('lib/emoticons.yml').each { |meaning, faces|
+    emoticons[meaning] = {
+      :english => faces[0],
+      :japanese => faces[1]
+    }
+  }
+  emoticons
 end
 
 def get_japanese_emoticon
